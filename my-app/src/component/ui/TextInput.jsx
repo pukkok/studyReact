c980f11b1f75d29e2609a6014ed1290272dfd93f
Pukkok <krystalgggg@gmail.com>
2024-02-24 15:38:58 +0900

    textInput 수정

--- my-app/src/component/ui/TextInput.jsx
+++ my-app/src/component/ui/TextInput.jsx
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 
 const StyledTextarea = styled.textarea`
     width: calc(100% -32px);
-    ${(props) =>
-    props.height &&
+    ${(props) => props.height &&
     `height: ${props.height}px;`
     }
     padding: 16px;
